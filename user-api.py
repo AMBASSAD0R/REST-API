de6069bb9ae328227ac0fc6008f21b19d748d998
AMBASSAD0R <44470355+AMBASSAD0R@users.noreply.github.com>
2021-04-25 21:28:14 +0300

    Update user-api.py

--- user-api.py
+++ user-api.py
@@ -8,3 +8,9 @@ blueprint = flask.Blueprint(
     __name__,
     template_folder='templates'
 )
+
+
+@blueprint.route('/api/users', methods=['GET'])
+def get_users():
+    db = db_session.create_session()
+    return jsonify({'users': [item.to_dict(only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'hashed_password', 'modified', 'city_from')) for item in db.query(User).all()]})
